d8aedc699e32099b2129d21fe6b75c557f70d212
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-25 18:28:44 +0100

    Implement dropdown button

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF455A64),
+//        primaryColor: Color(0xFF455A64),
         scaffoldBackgroundColor: Colors.white,
       ),
       home: PriceScreen(),
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +44,23 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Color(0xFFFFC107),
-            child: null,
+            child: DropdownButton(
+                value: selectedCurrency,
+                items: [
+                  DropdownMenuItem(
+                    child: Text('USD'),
+                    value: 'USD',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('EUR'),
+                    value: 'EUR',
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    selectedCurrency = value;
+                  });
+                }),
           ),
         ],
       ),
